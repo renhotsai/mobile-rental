01ead024a8bb54c6d1fdb95bc1a1e860b1c0f85f
ichahal <aman.0002@icloud.com>
2023-11-21 16:53:26 -0500

    updated gradle to add view binding and compile sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hy.group3_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hy.group3_project"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
