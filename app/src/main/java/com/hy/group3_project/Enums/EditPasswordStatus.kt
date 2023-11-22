36aadaf4d0895e8341d88361817eb5f4b2d8dc49
Jeremy <renhotsai@gmail.com>
2023-11-21 20:29:00 -0500

    add enums

---
+++ app/src/main/java/com/hy/group3_project/Enums/EditPasswordStatus.kt
@@ -0,0 +1,10 @@
+package com.hy.group3_project.Enums
+
+enum class EditPasswordStatus {
+    Success,
+    CurrentPasswordEmpty,
+    NewPasswordEmpty,
+    ConfirmPasswordEmpty,
+    CurrentPasswordWrong,
+    ConfirmPasswordDifferent
+}
