bac05a02ffe2e61acbbdb232467ccace81df26b5
Jeremy <renhotsai@gmail.com>
2023-11-21 22:11:05 -0500

    edit MainActivity

--- app/src/main/java/com/hy/group3_project/MainActivity.kt
+++ app/src/main/java/com/hy/group3_project/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.hy.group3_project
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hy.group3_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var perfEditor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        this.sharedPreferences = getSharedPreferences("MY_APP_PREFS", MODE_PRIVATE)
+        this.perfEditor = this.sharedPreferences.edit()
+
     }
 }
