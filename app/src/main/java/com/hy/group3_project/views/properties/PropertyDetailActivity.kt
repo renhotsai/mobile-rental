7e62bda587320e8d753fda16551308542ea88839
HyunSeongLee <devhyun05@gmail.com>
2023-12-12 20:19:35 -0500

    Details page check street view

--- app/src/main/java/com/hy/group3_project/views/properties/PropertyDetailActivity.kt
+++ app/src/main/java/com/hy/group3_project/views/properties/PropertyDetailActivity.kt
@@ -2,9 +2,11 @@ package com.hy.group3_project.views.properties
 
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -27,7 +29,8 @@ class PropertyDetailActivity : BaseActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityPropertyDetailBinding
     private var mMap: GoogleMap? = null
     private var selectedProperty: Property? = null
-
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +38,7 @@ class PropertyDetailActivity : BaseActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         this.propertyRepository = PropertyRepository(applicationContext)
-//set option menu
+
 
 
         //set option menu
@@ -107,6 +110,21 @@ class PropertyDetailActivity : BaseActivity(), OnMapReadyCallback {
                 finish()
             }
         }
+
+        binding.btnShowStreetView.setOnClickListener {
+            Log.d(TAG, "latitude: $latitude")
+            val streetViewUri = Uri.parse("google.streetview:cbll=${"$latitude"},${"$longitude"}&cbp=1,0,,0,1.0")
+            val streetViewIntent = Intent(Intent.ACTION_VIEW, streetViewUri)
+            streetViewIntent.setPackage("com.google.android.apps.maps")
+
+            if (streetViewIntent.resolveActivity(packageManager) != null) {
+                startActivity(streetViewIntent)
+            } else {
+                // Handle the case where Google Maps app is not installed
+                Toast.makeText(this, "Google Maps app is not installed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -116,9 +134,13 @@ class PropertyDetailActivity : BaseActivity(), OnMapReadyCallback {
     private fun addMarker(property: Property) {
         if (mMap != null) {
             val address = getAddress(property.address)
-            val latLng = address?.let { LatLng(it.latitude, it.longitude) }
 
-            if (latLng != null) {
+            if (address != null) {
+                latitude = address.latitude
+                longitude = address.longitude
+
+                val latLng = LatLng(latitude, longitude)
+
                 Log.d(TAG, "Add marker $latLng")
                 mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f))
                 mMap!!.addMarker(MarkerOptions().position(latLng).title(property.address))
@@ -131,6 +153,7 @@ class PropertyDetailActivity : BaseActivity(), OnMapReadyCallback {
             // You can display a message to the user or perform any other action here
         }
     }
+
 }
 
 
